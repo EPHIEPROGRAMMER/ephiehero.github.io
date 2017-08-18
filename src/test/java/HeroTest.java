48c619f0305a941a9187e4b2014cfbab98ac7e86
ephie <ephie@localhost.localdomain>
2017-08-18 16:05:03 +0300

    created the Hero & Squad classes and set the variables,getters and setters

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -6,58 +6,58 @@ import java.time.LocalDateTime;
 
    @Test
    public void newHero_instantiatesCorrectly() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
      assertEquals(true, testHero instanceof Hero);
    }
 
    @Test
    public void getName_getsHeroName_String() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-     assertEquals("Scorpion", testHero.getName());
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
+     assertEquals("githeriman", testHero.getName());
    }
 
    @Test
    public void getAge_getsHeroAge_int() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-     assertEquals(42, testHero.getAge());
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
+     assertEquals(32, testHero.getAge());
    }
 
 
    @Test
    public void getStrength_getsHeroStrength_String() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-     assertEquals("Hellfire", testHero.getStrength());
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
+     assertEquals("githeri", testHero.getStrength());
    }
 
    @Test
    public void getWeakness_getsHeroWeakness_String() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-     assertEquals("Subzero", testHero.getWeakness());
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
+     assertEquals("pizza", testHero.getWeakness());
    }
 
    @Test
    public void hasConcededDefeat_isFalseAfterInstantiation_false() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
      assertEquals(false, testHero.hasConcededDefeat());
    }
 
    @Test
    public void getCreatedAt_instantiatesWithCurrentTime_today() {
-     Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
+     Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
      assertEquals(LocalDateTime.now().getDayOfWeek(), testHero  .getCreatedAt().getDayOfWeek());
    }
 
    @Test
    public void all_returnsAllInstancesOfHero_true() {
-    Hero firstHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-    Hero secondHero = new Hero("Raiden", 1000, "Lightning", "Shinnok");
+    Hero firstHero = new Hero("githeriman", 32, "githeri", "pizza");
+    Hero secondHero = new Hero("saltbae", 35, "salt", "sugar");
     assertEquals(true, Hero.all().contains(firstHero));
     assertEquals(true, Hero.all().contains(secondHero));
   }
 
   @Test
   public void clear_emptiesAllHeroesFromArrayList_0() {
-    Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
+    Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
     Hero.clear();
     assertEquals(Hero.all().size(), 0);
   }
@@ -65,14 +65,14 @@ import java.time.LocalDateTime;
   @Test
   public void getId_heroesInstantiateWithAnID_1() {
     Hero.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!
-    Hero testHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
+    Hero testHero = new Hero("githeriman", 32, "githeri", "pizza");
     assertEquals(1, testHero.getId());
   }
 
   @Test
   public void find_returnsHeroWithSameId_secondHero() {
-    Hero firstHero = new Hero("Scorpion", 42, "Hellfire", "Subzero");
-    Hero secondHero = new Hero("Raiden", 1000, "Lightning", "Shinnok");
+    Hero firstHero = new Hero("githeriman", 32, "githeri", "pizza");
+    Hero secondHero = new Hero("saltbae", 35, "salt", "sugar");
     assertEquals(Hero.find(secondHero.getId()), secondHero);
   }
  }
---
+++ src/test/java/SquadTest.java
@@ -0,0 +1,76 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class SquadTest {
+
+  // @Test
+  // public void name_addsHeroToGroupAndGetsHeroName_String() {
+  //   Squad testSquad = new Squad("Squad A");
+  //   Hero testHero = new Hero("githeriman", 32, "githeri", "Subzero");
+  //   testSquad.addHero(testHero);
+  //   assertEquals("githeriman", testSquad.heroName(0));
+  // }
+
+  // @Test
+  // public void name_getsGroupOfSquad_String() {
+  //   Squad testSquad = new Squad("Squad A");
+  //   Hero testHero = new Hero("githeriman", 32, "githeri", "Subzero");
+  //   Hero testHero2 = new Hero("saltbae", 47, "salt", "sugar");
+  //   testSquad.addHero(testHero);
+  //   testSquad.addHero(testHero2);
+  //   String listOfHeroes = testSquad.heroName(0) + " " + testSquad.heroName(1);
+  //   assertEquals("githeriman saltbae", listOfHeroes);
+  // }
+
+  @Test
+  public void all_returnsAllInstancesOfSquad_true() {
+   Squad firstSquad = new Squad("Squad A");
+   Squad secondSquad = new Squad("Squad B");
+   assertEquals(true, Squad.all().contains(firstSquad));
+   assertEquals(true, Squad.all().contains(secondSquad));
+ }
+
+  @Test
+  public void clear_emptiesAllCategoriesFromList_0() {
+    Squad testSquad = new Squad("Squad A");
+    Squad.clear();
+    assertEquals(Squad.all().size(), 0);
+  }
+
+  @Test
+  public void getId_squadsInstantiateWithAnId_1() {
+   Squad testSquad = new Squad("Squad A");
+   assertEquals(1, testSquad.getId());
+ }
+
+ @Test
+ public void find_returnsSquadWithSameId_secondSquad() {
+   Squad.clear();
+   Squad firstSquad = new Squad("Squad A");
+   Squad secondSquad = new Squad("Squad B");
+   assertEquals(Squad.find(secondSquad.getId()), secondSquad);
+ }
+
+ @Test
+ public void getHeroes_initiallyReturnsEmptyList_ArrayList() {
+  Squad.clear();
+  Squad testSquad = new Squad("Squad A");
+  assertEquals(0, testSquad.getHeroes().size());
+}
+
+@Test
+public void addHero_addsHeroToList_true() {
+  Squad testSquad = new Squad("Squad A");
+  Hero testHero = new Hero("githeriman", 32, "githeri", "Subzero");
+  testSquad.addHero(testHero);
+  assertTrue(testSquad.getHeroes().contains(testHero));
+}
+
+@Test
+  public void find_returnsNullWhenNoTaskFound_null() {
+    assertTrue(Squad.find(999) == null);
+  }
+}
